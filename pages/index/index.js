805011dafa23e2456dc4e6002018d9786574c945
shenyibo <1335638583@qq.com>
2019-11-01 17:44:51 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,24 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    btuBottom: 0,
+    statusBarHeight: 0
+  },
+  onLoad: function () {
+    this.setData({
+      btuBottom: app.globalData.statusBarHeight + "px",
+      statusBarHeight: ((app.globalData.statusBarHeight * 2) + 100) + "rpx"
+    })
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
